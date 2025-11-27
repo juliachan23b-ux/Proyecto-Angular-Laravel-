af0b3e10f0a566ce412687a2e433d014cf530265
JuliaChan23b-UX <julia.chan23b@udgvirtual.udg.mx>
2025-11-27 10:13:29 -0600

    Fix root route to avoid 500 in Render

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,7 +4,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['status' => 'Backend funcionando'];
 });
 
 // Ruta para servir la app Angular
